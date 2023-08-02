6bf9ff6de96443c550e696cbfa16020a26779ff7
nooose <hd15807@gmail.com>
2023-08-02 18:28:51 +0900

    CORS 허용 ORIGIN 추가

--- src/main/java/com/megazone/springbootboilerplate/config/web/WebConfig.java
+++ src/main/java/com/megazone/springbootboilerplate/config/web/WebConfig.java
@@ -3,15 +3,23 @@ package com.megazone.springbootboilerplate.config.web;
 import com.megazone.springbootboilerplate.config.web.interceptor.LogInterceptor;
 import com.megazone.springbootboilerplate.config.web.interceptor.MetricInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class InterceptorConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
+
+    public static final String[] ALLOWED_ORIGINS = {"http://front-end.com"};
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LogInterceptor()).order(1);
         registry.addInterceptor(new MetricInterceptor()).order(2);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(ALLOWED_ORIGINS);
     }
 }
