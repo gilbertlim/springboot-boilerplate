aa0ddc6eeae6323b6dfe94a628f100c44685ba79
nooose <hd15807@gmail.com>
2023-08-18 20:02:28 +0900

    테스트 컨텍스트 마다 DB 초기화
    테스트가 계속 실패하여 수정함

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,26 +1,29 @@
-CREATE TABLE shop
+ DROP TABLE IF EXISTS shop CASCADE;
+CREATE TABLE IF NOT EXISTS shop
 (
-    shop_id   BIGINT PRIMARY KEY AUTO_INCREMENT,
-    shop_name VARCHAR(255) NOT NULL,
-    shop_address VARCHAR(255) NOT NULL,
+    shop_id           BIGINT PRIMARY KEY AUTO_INCREMENT,
+    shop_name         VARCHAR(255) NOT NULL,
+    shop_address      VARCHAR(255) NOT NULL,
     shop_detail_address VARCHAR(255) NOT NULL,
-    shop_tier VARCHAR(255) NOT NULL,
-    shop_tier_code VARCHAR(255) NOT NULL
+    shop_tier         VARCHAR(255) NOT NULL,
+    shop_tier_code    VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE review
+DROP TABLE IF EXISTS review CASCADE;
+CREATE TABLE IF NOT EXISTS review
 (
-    review_id   BIGINT PRIMARY KEY AUTO_INCREMENT,
-    review_contents VARCHAR(255) NOT NULL,
-    shop_id     BIGINT,
+    review_id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    review_contents   VARCHAR(255) NOT NULL,
+    shop_id           BIGINT,
     FOREIGN KEY (shop_id) REFERENCES shop(shop_id)
 );
 
-CREATE TABLE shedlock
+DROP TABLE IF EXISTS shedlock CASCADE;
+CREATE TABLE IF NOT EXISTS shedlock
 (
-    name       VARCHAR(64),
-    lock_until TIMESTAMP(3) NULL,
-    locked_at  TIMESTAMP(3) NULL,
-    locked_by  VARCHAR(255),
+    name              VARCHAR(64),
+    lock_until        TIMESTAMP NULL,
+    locked_at         TIMESTAMP NULL,
+    locked_by         VARCHAR(255),
     PRIMARY KEY (name)
-)
+);
