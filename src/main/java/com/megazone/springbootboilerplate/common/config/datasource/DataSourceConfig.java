d01360827d6257e84215b93dcd22c07c82964d8a
nooose <hd15807@gmail.com>
2023-08-23 22:10:27 +0900

    데이터소스 Bean 생성 코드 리팩토링

--- src/main/java/com/megazone/springbootboilerplate/common/config/datasource/DataSourceConfig.java
+++ src/main/java/com/megazone/springbootboilerplate/common/config/datasource/DataSourceConfig.java
@@ -1,40 +1,50 @@
 package com.megazone.springbootboilerplate.common.config.datasource;
 
+import com.zaxxer.hikari.HikariConfig;
 import jakarta.annotation.PostConstruct;
 import java.util.Map;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.config.BeanDefinitionCustomizer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
+@Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class DataSourceConfig {
+
+    private static final String DATA_SOURCE_SUFFIX = "DataSource";
 
     private final DataSourceProperties dataSourceProperties;
     private final GenericApplicationContext applicationContext;
 
     @PostConstruct
-    public void initDataSource() {
+    public void init() {
         for (var entry : dataSourceProperties.groups().entrySet()) {
-            DataSource dataSource = DataSourceFactory.generateDataSource(entry.getKey(), entry.getValue(), dataSourceProperties.hikari());
-            initializeDataSource(entry, dataSource);
+            String key = entry.getKey();
+            var dataSourceInfo = entry.getValue();
+            HikariConfig defaultHikariConfig = dataSourceProperties.hikari();
+            DataSource dataSource = DataSourceFactory.generateDataSource(key, dataSourceInfo, defaultHikariConfig);
+            registerDataSourceBean(entry, dataSource);
         }
     }
 
-    private void initializeDataSource(Map.Entry<String, DataSourceProperties.FlexibleDataSourceInfo> dataSourceInfoEntry, DataSource dataSource) {
-        String beanName = dataSourceInfoEntry.getKey() + "DataSource";
+    private void registerDataSourceBean(Map.Entry<String, DataSourceProperties.FlexibleDataSourceInfo> dataSourceInfoEntry, DataSource dataSource) {
+        String beanName = dataSourceInfoEntry.getKey() + DATA_SOURCE_SUFFIX;
         var dataSourceInfo = dataSourceInfoEntry.getValue();
+        registerDataSourceBeanWithCustomization(dataSource, dataSourceInfo, beanName);
+    }
+
+    private void registerDataSourceBeanWithCustomization(DataSource dataSource, DataSourceProperties.FlexibleDataSourceInfo dataSourceInfo, String beanName) {
         if (dataSourceInfo.isCluster()) {
+            applicationContext.getBeanFactory().initializeBean(dataSource, beanName);
             applicationContext.registerBean(beanName, LazyConnectionDataSourceProxy.class, primaryBeanCustomizer(dataSourceInfo, dataSource));
-            initializeBean(beanName, dataSource);
             return;
         }
-
         applicationContext.registerBean(beanName, DataSource.class, () -> dataSource, primaryBeanCustomizer(dataSourceInfo, dataSource));
-        initializeBean(beanName, dataSource);
     }
 
     private static BeanDefinitionCustomizer primaryBeanCustomizer(DataSourceProperties.FlexibleDataSourceInfo dataSourceInfo, DataSource dataSource) {
@@ -45,10 +55,5 @@ public class DataSourceConfig {
                 beanDefinition.setPrimary(true);
             }
         };
-    }
-
-    private void initializeBean(String beanName, DataSource dataSource) {
-        var beanFactory = applicationContext.getBeanFactory();
-        beanFactory.initializeBean(dataSource, beanName);
     }
 }
