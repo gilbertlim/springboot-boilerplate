4e6dc4754b0c117764a51b1e1d9517f7ea4f2c55
gilbertlim <9ilbert.app@gmail.com>
2023-08-23 17:19:47 +0900

    submodule script 수정

--- git_submodule.sh
+++ git_submodule.sh
@@ -2,6 +2,8 @@
 
 # submodule 적용
 # git submodule add [REPO_URL] [DIRECTORY]
+rm -rf .git/modules
+rm -rf src/main/java/com/megazone/springbootboilerplate/common_module
 git submodule add git@github.com:gilbertlim/springboot-boilerplate-common.git src/main/java/com/megazone/springbootboilerplate/common_module
 
 # submodule 최신화
