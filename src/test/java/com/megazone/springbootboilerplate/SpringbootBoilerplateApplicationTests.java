c835031cda3f7b7c7c5442fb01ce82a3c1257065
gilbertlim <9ilbert.app@gmail.com>
2023-09-17 21:59:51 +0900

    initialize

---
+++ src/test/java/com/megazone/springbootboilerplate/SpringbootBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megazone.springbootboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBoilerplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
