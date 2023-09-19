bc2f62de4b621a3e17669c87f5354fb46db8044b
gilbertlim <9ilbert.app@gmail.com>
2023-09-19 11:27:42 +0900

    thread pool 설정

---
+++ src/main/java/com/boilerplate/common/config/task/TaskExecutorConfig.java
@@ -0,0 +1,25 @@
+package com.boilerplate.common.config.task;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class TaskExecutorConfig {
+
+    //@Bean
+    public TaskExecutor defaultTaskExecutor() {
+        ThreadPoolTaskExecutor threadPoolExecutor = new ThreadPoolTaskExecutor();
+        threadPoolExecutor.setCorePoolSize(20);
+        threadPoolExecutor.setMaxPoolSize(50);
+        threadPoolExecutor.setQueueCapacity(100);
+        //threadPoolExecutor.setThreadNamePrefix("task-");
+        threadPoolExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        threadPoolExecutor.setAwaitTerminationSeconds(30);
+        //threadPoolExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        threadPoolExecutor.initialize();
+        return threadPoolExecutor;
+    }
+}
